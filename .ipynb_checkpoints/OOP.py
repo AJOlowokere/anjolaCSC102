7a80a631cc4b3f4a531e5d2877a705a651db4cb1
AJOlowokere <runse.aj@gmail.com>
2021-05-20 15:48:42 +0100

    More OOP

--- .ipynb_checkpoints/OOP.py
+++ .ipynb_checkpoints/OOP.py
@@ -1,10 +1,17 @@
 class Coffee:
-
+    coffeeCupCounter = 0
     def __init__(self, themilk, thesugar, thecoffeemate):
         self.milk = themilk
         self.sugar = thesugar
         self.coffeemate = thecoffeemate
+        Coffee.coffeeCupCounter = Coffee.coffeeCupCounter +1
         print(f"You now have your coffee with {self.milk} milk, {self.sugar} sugar {self.coffeemate} coffeemate")
 
 mySugarFreeCoffee = Coffee(2,0,1)
+print(mySugarFreeCoffee.sugar)
 myMuchSugarCoffee = Coffee(2, 10, 1)
+print(myMuchSugarCoffee.sugar)
+print(f"we have made {Coffee.coffeeCupCounter} coffee cups so farr!")
+print(f"we have made {mySugarFreeCoffee.coffeeCupCounter} coffee cups so farr!")
+print(f"we have made {myMuchSugarCoffee.milk} coffee cups so farr!")
+print(f"we have made {myMuchSugarCoffee.coffeeCupCounter} coffee cups so farr!")
